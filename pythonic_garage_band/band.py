5848a859264cfbad8da997799bdde97440a7a443
Alex Guo <guojiarui@gmail.com>
2022-08-08 00:59:12 -0700

    all 16 tests passed.

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -1,4 +1,3 @@
-
 class Band:
     instances = []
 
@@ -13,27 +12,73 @@ class Band:
             solos.append(member.play_solo())
         return tuple(solos)
 
+    @classmethod
+    def to_list(cls):
+        return cls.instances
+
+    def __repr__(self):
+        return
+
+    def __str__(self):
+        return
+
+
 class Musician:
     def __init__(self, name):
         self.name = name
 
+
 class Guitarist(Musician):
+    def __init__(self, name):
+        self.name = name
+        self.instrument = "guitar"
+
+    def __str__(self):
+        return f"My name is {self.name} and I play {self.instrument}"
+
+    def __repr__(self):
+        return f"Guitarist instance. Name = {self.name}"
+
     def get_instrument(self):
         return 'guitar'
 
     def play_solo(self):
         return 'face melting guitar solo'
 
+    def get_instrument(self):
+        return self.instrument
+
 
 class Bassist(Musician):
+
+    def __init__(self, name):
+        self.name = name
+        self.instrument = "bass"
+
+    def __str__(self):
+        return f"My name is {self.name} and I play {self.instrument}"
+
+    def __repr__(self):
+        return f"Bassist instance. Name = {self.name}"
+
     def get_instrument(self):
         return 'bass'
 
     def play_solo(self):
         return 'bom bom buh bom'
-
+#
 
 class Drummer(Musician):
+    def __init__(self, name):
+        self.name = name
+        self.instrument = "drums"
+
+    def __str__(self):
+        return f"My name is {self.name} and I play {self.instrument}"
+
+    def __repr__(self):
+        return f"Drummer instance. Name = {self.name}"
+
     def get_instrument(self):
         return 'drums'
 
